a388986d22df2893ead807c23cd23495076cb380
Charlie Greenman <charlie@razroo.com>
2023-08-27 21:14:15 -0700

    Create starter files for angular-16.2.0

---
+++ apps/test-codegen-nine/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-codegen-nine-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-codegen-nine';
+}
